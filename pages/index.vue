2c13057fdd464376e98f806c7d639ac3b830689b
ryancommits <qwang1993@gmail.com>
2018-01-20 18:15:28 -0500

    Finish HTML content and basic CSS styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,35 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        portfolio
+  <section class="index__background">
+    <div class="index__container">
+      <h1>
+        <nuxt-link class="index__name" to="/">Ryan Wang</nuxt-link>
       </h1>
-      <h2 class="subtitle">
-        Ryan's portfolio
+      <h2 class="index__title">
+        Ryan is a front-end web developer.
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <p class="index__status">
+        <span class="index__email">Available</span> to join a kickass front-end team.
+      </p>
+
+      <nav>
+        <ul class="index__list">
+          <li class="index__link">
+            <a>Work</a>
+          </li>
+          <li class="index__link">
+            <a>Resume</a>
+          </li>
+          <li class="index__link">
+            <a>Medium</a>
+          </li>
+          <li class="index__link">
+            <a>Github</a>
+          </li>
+          <li class="index__link">
+            <a>LinkedIn</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </section>
 </template>
@@ -33,22 +45,44 @@ export default {
 </script>
 
 <style>
-.container {
+.index__background {
+  min-width: 100vw;
   min-height: 100vh;
+  background-color: #181818;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
 }
-
-.title {
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.index__container {
+  width: 600px;
 }
-
+.index__name {
+  text-decoration: none;
+  color: white;
+  font-size: 20px;
+}
+.index__title {
+  color: white;
+  font-size: 50px;
+  margin: 50px 0px;
+}
+.index__status {
+  color: white;
+  font-size: 25px;
+  margin-bottom: 60px;
+}
+.index__email {
+  color: #6d6d6d;
+}
+.index__list {
+  list-style: none;
+  padding: 0;
+}
+.index__link {
+  display: inline-block;
+  color: #6d6d6d;
+  margin-right: 10px;
+}
 .subtitle {
   font-weight: 300;
   font-size: 42px;
